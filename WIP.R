41ac53917da8b0c4aee4139b183c66273a85860a
JaceInnis <79427747+JaceInnis@users.noreply.github.com>
2022-07-28 14:41:04 -0500

    fist push. Gets dates in ready format

---
+++ WIP.R
@@ -0,0 +1,55 @@
+library(lubridate)
+
+data = read.csv("final.csv")
+
+data$date = dmy(data$date)
+
+start = ymd("2012-01-06")
+
+end = ymd("2022-03-31")
+
+ndays = (((year(end)-2012)*365)+yday(end))
+
+
+for (i in 1:ndays) {
+
+  if(i == 1){
+    dfram = data.frame(date = NA, site = NA)
+  }
+
+x = start + (i-1)
+  
+dfram[i,1] = x
+
+ for (j in 1:nrow(data)) {
+   if(
+     x >= data$date[j] & x < data$date[j+1]
+   ){
+     w = data$C[j]
+   }
+ }
+
+dfram[i,2] = w   
+}
+
+
+tail(dfram)
+
+ymd(dfr)
+
+data[1,2]
+
+i1 = start
+i2 = start+7
+
+
+
+((year(data$date[100])-2012)*365)+yday(data$date[100])
+
+
+
+
+
+
+
+
