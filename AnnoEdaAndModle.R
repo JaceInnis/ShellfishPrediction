40cce2de7f7af17ed23222bea41ac69b2c6cbcd1
JaceInnis <79427747+JaceInnis@users.noreply.github.com>
2022-08-27 11:49:10 -0500

    AnnoModel
    The location scripts makes a location master list that contains Anno model

---
+++ AnnoEdaAndModle.R
@@ -0,0 +1,105 @@
+library(tidyverse)
+library(lubridate)
+
+
+mega = read.csv("finalAnno.CSV")
+
+
+
+ mega = mega[! (mega$Location == "Mindoro" & mega$Variable == "Sal" & mega$Layer ==0 & mega$Value<13000),]
+ mega = mega[! (mega$Location == "Mindoro" & mega$Variable == "Velu" & mega$Layer ==0 & mega$Value>1500),]
+ mega = mega[! (mega$Location == "Mindoro" & mega$Variable == "Velv" & mega$Layer ==0 & mega$Value>1000),]
+ mega = mega[! (mega$Location == "sibuyan" & mega$Variable == "Sal" & mega$Layer ==0 & mega$Value<13000),]
+ mega = mega[! (mega$Location == "sibuyan" & mega$Variable == "Velu" & mega$Layer ==0 & mega$Value>1000),]
+ mega = mega[! (mega$Location == "sibuyan" & mega$Variable == "Velv" & mega$Layer ==0 & mega$Value>1000),]
+ mega = mega[! (mega$Location == "southsibuyan" & mega$Variable == "Sal" & mega$Layer ==0 & mega$Value<12500),]
+ mega = mega[! (mega$Location == "southsibuyan" & mega$Variable == "Velu" & mega$Layer ==0 & mega$Value>1000),]
+ mega = mega[! (mega$Location == "southsibuyan" & mega$Variable == "Velv" & mega$Layer ==0 & mega$Value>1000),]
+ mega = mega[! (mega$Location == "surigao" & mega$Variable == "Sal" & mega$Layer ==0 & mega$Value<12500),]
+ mega = mega[! (mega$Location == "surigao" & mega$Variable == "Velu" & mega$Layer ==0 & mega$Value>1000),]
+ mega = mega[! (mega$Location == "surigao" & mega$Variable == "Velv" & mega$Layer ==0 & mega$Value>1000),]
+ mega = mega[! (mega$Location == "tablas" & mega$Variable == "Sal" & mega$Layer ==0 & mega$Value<12500),]
+ mega = mega[! (mega$Location == "tablas" & mega$Variable == "Velu" & mega$Layer ==0 & mega$Value>1000),]
+ mega = mega[! (mega$Location == "tablas" & mega$Variable == "Velv" & mega$Layer ==0 & mega$Value>1000),]
+ mega = mega[! (mega$Location == "Visayan" & mega$Variable == "Sal" & mega$Layer ==0 & mega$Value<12500),]
+ mega = mega[! (mega$Location == "Visayan" & mega$Variable == "Velu" & mega$Layer ==0 & mega$Value>1000),]
+ mega = mega[! (mega$Location == "Visayan" & mega$Variable == "Velv" & mega$Layer ==0 & mega$Value>1000),]
+ 
+ 
+ mega = mega[! (mega$Location == "Mindoro" & mega$Variable == "Sal" & mega$Layer ==1 & mega$Value<13000),]
+ mega = mega[! (mega$Location == "Mindoro" & mega$Variable == "Temp" & mega$Layer ==1 & mega$Value>10),]
+ mega = mega[! (mega$Location == "Mindoro" & mega$Variable == "Velu" & mega$Layer ==1 & mega$Value<(-1000)),]
+ mega = mega[! (mega$Location == "Mindoro" & mega$Variable == "Velv" & mega$Layer ==1 & mega$Value>1000),]
+ mega = mega[! (mega$Location == "sibuyan" & mega$Variable == "Sal" & mega$Layer ==1 & mega$Value<14000),]
+ mega = mega[! (mega$Location == "sibuyan" & mega$Variable == "Velu" & mega$Layer ==1 & mega$Value<(-1000)),]
+ mega = mega[! (mega$Location == "sibuyan" & mega$Variable == "Velv" & mega$Layer ==1 & mega$Value>1000),]
+ mega = mega[! (mega$Location == "southsibuyan" & mega$Variable == "Sal" & mega$Layer ==1 & mega$Value<14000),]
+ mega = mega[! (mega$Location == "southsibuyan" & mega$Variable == "Velu" & mega$Layer ==1 & mega$Value<(-1000)),]
+ mega = mega[! (mega$Location == "southsibuyan" & mega$Variable == "Velv" & mega$Layer ==1 & mega$Value>1000),]
+ mega = mega[! (mega$Location == "surigao" & mega$Variable == "Sal" & mega$Layer ==1 & mega$Value<12000),]
+ mega = mega[! (mega$Location == "surigao" & mega$Variable == "Velu" & mega$Layer ==1 & mega$Value<(-2000)),]
+ mega = mega[! (mega$Location == "surigao" & mega$Variable == "Velv" & mega$Layer ==1 & mega$Value>1000),]
+ mega = mega[! (mega$Location == "tablas" & mega$Variable == "Sal" & mega$Layer ==1 & mega$Value<12500),]
+ mega = mega[! (mega$Location == "tablas" & mega$Variable == "Velu" & mega$Layer ==1 & mega$Value<(-1000)),]
+ mega = mega[! (mega$Location == "tablas" & mega$Variable == "Velv" & mega$Layer ==1 & mega$Value>1000),]
+
+ 
+
+
+
+
+
+
+
+mega$date = mdy(mega$date)
+mega$day = yday(mega$date)
+
+
+locs = names(table(mega$Location))
+vars = names(table(mega$Variable))
+
+
+
+  for (w in 1:5) {
+    for (x in 1:4) {
+      mega %>% 
+        filter(Location == locs[w], Variable == vars[x], Layer == 1) -> gra
+      
+      plot(Value~day,data=gra,xlab = vars[x], main = locs[w])
+    }
+  }
+
+#  "Mindoro"      "sibuyan"      "southsibuyan" "surigao"      "tablas"       "Visayan"     
+#   "Sal"         "Temp"          "Velu"        "Velv"
+
+
+w = 1    # location
+x = 4   # var
+
+
+
+mega %>% 
+  filter(Location == locs[w], Variable == vars[x], Layer == 0) -> gra
+
+plot(Value~day, data = gra, xlab = vars[x], main = locs[w])
+
+
+annolm <- lm(Value  ~ sin((2*pi/365)*day)+cos((2*pi/365)*day)
+            +sin((4*pi/365)*day)+cos((4*pi/365)*day),data=gra)
+
+annolm <- lm(Value  ~ sin((2*pi/365)*day)+cos((2*pi/365)*day),data=gra)
+
+
+
+mod = predict(annolm, data.frame(day = 1:365))
+
+lines(1:365,mod,col=2)
+
+mod = as.data.frame(mod)$mod
+
+
+lines = cbind(lines, mindorovelv = mod)
+
+
+write.csv(lines, "modeledAnno.csv")
+
--- AnnoSuperScript.R
+++ AnnoSuperScript.R
@@ -1,9 +1,59 @@
+library(RGEEtools)
+
+start()
+
+
+# -------------------------- collection function -------------------------
 
 
 
 
 
-mega = data.frame(date = NA, Location = NA, Layer = NA, Variable = NA, Value = NA)
+
+rob = function(x){
+  print("a")
+  
+  temp = wttolist(depth = 0, date = list, interval = 7, ROI = x[[1]], scale = x[[3]])
+  temp = mean((geetodf(temp)[,3]*0.001)+20)
+  addrowm = data.frame(date = as.character(list), Location = x[[5]], Layer = 0, Variable = "Temp", Value = temp)
+  
+
+  temp = wstolist(depth = 0, date = list, interval = 7, ROI = x[[1]], scale = x[[3]])
+  temp = mean((geetodf(temp)[,3]))
+  addrowm[2,] = data.frame(date = as.character(list), Location = x[[5]], Layer = 0, Variable = "Sal", Value = temp)
+  
+  temp = wvtolist(depth = 0, date = list, interval = 7, ROI = x[[1]], scale = x[[3]])
+  addrowm[3,] = data.frame(date = as.character(list), Location = x[[5]], Layer = 0, Variable = "Velu", Value = mean(geetodf(temp)[,3]))
+  addrowm[4,] = data.frame(date = as.character(list), Location = x[[5]], Layer = 0, Variable = "Velv", Value = mean(geetodf(temp)[,4]))
+  
+
+  if(x[[2]] == 1){
+    print("b")
+    
+    temp = wttolist(depth = x[[4]], date = list, interval = 7, ROI = x[[1]], scale = x[[3]])
+    temp = mean((geetodf(temp)[,3]*0.001)+20)
+    addrowm[5,] = data.frame(date = as.character(list), Location = x[[5]], Layer = 1, Variable = "Temp", Value = temp)
+    
+    
+    temp = wstolist(depth = x[[4]], date = list, interval = 7, ROI = x[[1]], scale = x[[3]])
+    temp = mean((geetodf(temp)[,3]))
+    addrowm[6,] = data.frame(date = as.character(list), Location = x[[5]], Layer = 1, Variable = "Sal", Value = temp)
+    
+    temp = wvtolist(depth = x[[4]], date = list, interval = 7, ROI = x[[1]], scale = x[[3]])
+    addrowm[7,] = data.frame(date = as.character(list), Location = x[[5]], Layer = 1, Variable = "Velu", Value = mean(geetodf(temp)[,3]))
+    addrowm[8,] = data.frame(date = as.character(list), Location = x[[5]], Layer = 1, Variable = "Velv", Value = mean(geetodf(temp)[,4]))
+    
+  }
+  return(addrowm)
+  }
+  
+
+
+
+# ----------------------- Colloction loop ---------------------
+
+
+mega = data.frame(date = "as.ch", Location = NA, Layer = NA, Variable = NA, Value = NA)
 
 date = ymd(20120106)
 
@@ -11,61 +61,21 @@ date = ymd(20120106)
 
 while (TRUE) {
   list = date + runif(1, 1, 3574)
+  print(as.character(list))
+
+  
+  for (i in 1:6) {
+    print(i)
+    rex = rob(MasterLoc[[i]])
+    mega = rbind(mega, rex)
+  }
   
   
-  temp = wttolist(depth = 0, date = list, interval = 7, ROI = a, scale = 8000)
-  temp = mean((geetodf(temp)$water_temp_0*0.001)+20)
-  mega[nrow(mega)+1,] = data.frame(list, "Visayan",1, "Temp", temp)
-  
-  temp = wttolist(depth = 0, date = list, interval = 7, ROI = a, scale = 8000)
-  temp = mean((geetodf(temp)$water_temp_0*0.001)+20)
-  mega[nrow(mega)+1,] = data.frame(list, "Visayan",1, "Temp", temp)
-  
-  temp = wttolist(depth = 0, date = list, interval = 7, ROI = a, scale = 8000)
-  temp = mean((geetodf(temp)$water_temp_0*0.001)+20)
-  mega[nrow(mega)+1,] = data.frame(list, "Visayan",1, "Temp", temp)
-  
-  temp = wttolist(depth = 0, date = list, interval = 7, ROI = a, scale = 8000)
-  temp = mean((geetodf(temp)$water_temp_0*0.001)+20)
-  mega[nrow(mega)+1,] = data.frame(list, "Visayan",1, "Temp", temp)
-  
-  temp = wttolist(depth = 0, date = list, interval = 7, ROI = a, scale = 8000)
-  temp = mean((geetodf(temp)$water_temp_0*0.001)+20)
-  mega[nrow(mega)+1,] = data.frame(list, "Visayan",1, "Temp", temp)
-  
-  temp = wttolist(depth = 0, date = list, interval = 7, ROI = a, scale = 8000)
-  temp = mean((geetodf(temp)$water_temp_0*0.001)+20)
-  mega[nrow(mega)+1,] = data.frame(list, "Visayan",1, "Temp", temp)
-  
-  temp = wttolist(depth = 0, date = list, interval = 7, ROI = a, scale = 8000)
-  temp = mean((geetodf(temp)$water_temp_0*0.001)+20)
-  mega[nrow(mega)+1,] = data.frame(list, "Visayan",1, "Temp", temp)
-  
-  temp = wttolist(depth = 0, date = list, interval = 7, ROI = a, scale = 8000)
-  temp = mean((geetodf(temp)$water_temp_0*0.001)+20)
-  mega[nrow(mega)+1,] = data.frame(list, "Visayan",1, "Temp", temp)
-  
-  temp = wttolist(depth = 0, date = list, interval = 7, ROI = a, scale = 8000)
-  temp = mean((geetodf(temp)$water_temp_0*0.001)+20)
-  mega[nrow(mega)+1,] = data.frame(list, "Visayan",1, "Temp", temp)
-  
-  temp = wttolist(depth = 0, date = list, interval = 7, ROI = a, scale = 8000)
-  temp = mean((geetodf(temp)$water_temp_0*0.001)+20)
-  mega[nrow(mega)+1,] = data.frame(list, "Visayan",1, "Temp", temp)
-  
-  temp = wttolist(depth = 0, date = list, interval = 7, ROI = a, scale = 8000)
-  temp = mean((geetodf(temp)$water_temp_0*0.001)+20)
-  mega[nrow(mega)+1,] = data.frame(list, "Visayan",1, "Temp", temp)
-  
-  temp = wttolist(depth = 0, date = list, interval = 7, ROI = a, scale = 8000)
-  temp = mean((geetodf(temp)$water_temp_0*0.001)+20)
-  mega[nrow(mega)+1,] = data.frame(list, "Visayan",1, "Temp", temp)
-  
-  print(nrow(red))
+  print((nrow(mega)/44))
 }
 
+mega
 
+write.csv(mega, "finalAnno.CSV")
 
-temp = wstolist(depth = 10, date = date, interval = 7, ROI = f, scale = 13000)
-nrow(geetodf(temp))
 
